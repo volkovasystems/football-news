a07717ca252d46e5e9e1310c78e8a7b9cc38de17
Richeve S. Bebedor <volkovasystems@gmail.com>
2015-07-03 16:49:03 +0800

    Pushed changes.

---
+++ client/script/notify.js
@@ -0,0 +1,152 @@
+angular.module( "Notify", [ "Event", "PageFlow" ] )
+
+	.constant( "INFO_TYPE", "info-type" )
+
+	.constant( "WARN_TYPE", "warn-type" )
+
+	.constant( "ERROR_TYPE", "error-type" )
+
+	.constant( "SUCCESS_TYPE", "success-type" )
+
+	.factory( "Notify", [
+		"INFO_TYPE",
+		"WARN_TYPE",
+		"ERROR_TYPE",
+		"SUCCESS_TYPE",
+		function factory(
+			INFO_TYPE,
+			WARN_TYPE,
+			ERROR_TYPE,
+			SUCCESS_TYPE
+		){
+			var Notify = React.createClass( {
+				"statics": {
+					"attach": function attach( scope, container ){
+						React.render( <Notify scope={ scope } />, container[ 0 ] );
+
+						return this;
+					},
+
+					"parseType": function parseType( type ){
+						if( ( /info/i ).test( type ) ){
+							return INFO_TYPE;
+						}
+
+						if( ( /warn/i ).test( type ) ){
+							return WARN_TYPE;
+						}
+
+						if( ( /error/i ).test( type ) ){
+							return ERROR_TYPE;
+						}
+
+						if( ( /success/i ).test( type ) ){
+							return SUCCESS_TYPE;
+						}
+					}
+				},
+
+				"getInitialState": function getInitialState( ){
+					return {
+						"notifyMessage": "",
+						"notifyType": INFO_TYPE
+					};
+				},
+
+				"onClickCloseNotify": function onClickCloseNotify( ){
+					this.scope.publish( "hide-notify" );
+				},
+
+				"attachAllComponentEventListener": function attachAllComponentEventListener( ){
+					var self = this;
+
+					this.scope.on( "notify",
+						function onNotify( message, type ){
+							self.setState( {
+								"notifyMessage": message,
+								"notifyType": Notify.parseType( type )
+							} );
+
+							self.scope.publish( "show-notify" );
+
+							self.timeout = setTimeout( function onTimeout( ){
+								var notifyElement = $( "*", self.refs.notifyComponent.getDOMNode( ) );
+								
+								notifyElement.fadeOut( 1000, 
+									function onFadeOut( ){
+										self.scope.publish( "hide-notify" );
+
+										notifyElement.fadeIn( "fast" );
+
+										clearTimeout( self.timeout );
+
+										self.timeout = null;
+									} );
+							}, 3000 );
+						} );
+
+					this.scope.on( "hide-notify",
+						function onHideNotify( ){
+							if( self.timeout ){
+								clearTimeout( self.timeout );
+
+								self.timeout = null;
+							}
+						} );
+				},
+
+				"componentWillMount": function componentWillMount( ){
+					this.scope = this.props.scope;
+
+					this.attachAllComponentEventListener( );
+				},
+
+				"render": function onRender( ){
+					var notifyMessage = this.state.notifyMessage;
+
+					var notifyType = this.state.notifyType;
+
+					return; //: @template: template/notify.html
+				},
+
+				"componentDidMount": function componentDidMount( ){
+					this.scope.broadcast( "notify-rendered" );	
+				}
+			} );
+
+			return Notify;
+		}
+	] )
+
+	.directive( "notify", [
+		"Event",
+		"PageFlow",
+		"Notify",
+		function directive( Event, PageFlow, Notify ){
+			return {
+				"restrict": "EA",
+				"scope": true,
+				"priority": 2,
+				"link": function onLink( scope, element, attributeSet ){
+					Event( scope );
+
+					PageFlow( scope, element, "notify" );
+
+					scope.on( "show-notify",
+						function onShowNotify( ){
+							scope.showPage( );
+						} );
+
+					scope.on( "hide-notify",
+						function onHideNotify( ){
+							scope.hidePage( );
+						} );
+
+					scope.publish( "hide-notify" );
+
+					Notify
+						.attach( scope, element );
+				}
+			};
+		}
+	] );
---
+++ client/script/spinner.js
@@ -0,0 +1,124 @@
+angular
+
+	.module( "Spinner", [ "Event", "PageFlow" ] )
+
+	.factory( "Spinner", [
+		function factory( ){
+			var Spinner = React.createClass( {
+				"statics": {
+					"attach": function attach( scope, container, namespace ){
+						var spinnerComponent = (
+							<Spinner 
+								scope={ scope } 
+								namespace={ namespace } />
+						);
+						
+						React.render( spinnerComponent, container[ 0 ] );
+
+						return this;
+					},
+				},
+
+				"attachAllComponentEventListener": function attachAllComponentEventListener( ){
+					var self = this;
+
+					var namespace = this.props.namespace;
+
+					this.scope.on( "show-spinner",
+						function onShowSpinner( thisNamespace ){
+							if( thisNamespace == namespace ){
+								$( ".spinner-component", self.getDOMNode( ) ).spin( );
+							}
+						} );
+
+					this.scope.on( "hide-spinner",
+						function onShowSpinner( thisNamespace ){
+							if( thisNamespace == namespace ){
+								$( ".spinner-component", self.getDOMNode( ) ).spin( false );
+							}
+						} );
+				},
+
+				"componentWillMount": function componentWillMount( ){
+					this.scope = this.props.scope;
+
+					this.attachAllComponentEventListener( );
+				},
+
+				"render": function onRender( ){
+					var namespace = this.props.namespace;
+
+					return; //: @template: template/spinner.html
+				},
+
+				"componentDidMount": function componentDidMount( ){
+					this.scope.publish( "spinner-rendered" );	
+				}
+			} );
+
+			return Spinner;
+		}
+	] )
+
+	.factory( "attachSpinner", [
+		"$rootScope",
+		"Event",
+		"PageFlow",
+		"Spinner",
+		function factory( $rootScope, Event, PageFlow, Spinner ){
+			var attachSpinner = function attachSpinner( optionSet ){
+				var element = optionSet.element;
+
+				if( _.isEmpty( element ) || element.length == 0 ){
+					throw new Error( "unable to attach component" );
+				}
+
+				var scope = optionSet.scope || $rootScope;
+
+				var namespace = optionSet.namespace;
+
+				Event( scope );
+
+				PageFlow( scope, element, "spinner" );
+
+				scope.on( "show-spinner",
+					function onShowSpinner( thisNamespace ){
+						if( namespace == thisNamespace ){
+							scope.showPage( );
+						}
+					} );
+
+				scope.on( "hide-spinner",
+					function onHideSpinner( thisNamespace ){
+						if( namespace == thisNamespace ){
+							scope.hidePage( );	
+						}
+					} );
+
+				scope.publish( "hide-spinner", namespace );
+
+				Spinner.attach( scope, element, namespace );
+			};
+
+			return attachSpinner;
+		}
+	] )
+
+	.directive( "spinner", [
+		"attachSpinner",
+		function directive( attachSpinner ){
+			return {
+				"restrict": "EA",
+				"scope": true,
+				"priority": 3,
+				"link": function onLink( scope, element, attributeSet ){
+					attachSpinner( {
+						"scope": scope,
+						"element": element,
+						"attributeSet": attributeSet,
+						"namespace": scope.namespace || attributeSet.namespace
+					} );
+				}
+			}
+		}
+	] );
